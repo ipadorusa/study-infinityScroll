25544f99be3a6858d2af9fa88abf8ee6b06b2de1
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2019-12-23 00:30:01 +0900

    해보자 이저것

--- infinity.js
+++ infinity.js
@@ -1,18 +1,37 @@
 class infiteScroll {
   constructor(target) {
-    this.target = target;
+    this.target = document.querySelector(target);
+    this.nextCount = 0;
     this.init();
+  }
+  variable() {
+
   }
   init() {
     this.bindEvent();
+    
   }
   bindEvent() {
-    console.log('aa');
+    this.appendEvt();
+  }
+  observer() {
+    const io = new IntersectionObserver(callbakc, {
+      root: this.target,
+    });
+  }
+  appendEvt() {
+    for (let i=0;i<20;i++) {
+      const item = document.createElement('li');
+      item.innerText = 'Item ' + this.nextCount++;
+      this.target.appendChild(item);
+    }
   }
 }
 
-const scrollE = new infiteScroll();
+
 
 // 높이값
 // append 할 dom
 // append 할 list 갯수
+// createDocumentFragment
+// IntersectionObserver
